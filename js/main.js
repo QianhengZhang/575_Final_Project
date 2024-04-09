11ed372fbe0427cc93cb15af9a34282e8a6864f3
QianhengZhang <hazeland3427@gmail.com>
2024-04-09 05:19:30 -0500

    add initial packages and functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
 //insert code here!
+window.onload = setMap();
+
+
+function setMap(){
+    var height = window.innerHeight;
+    var width = window.innerWidth * 0.7;
+    map = L.map('map', {
+        center: [20, 0],
+        zoom: 2
+    });
+    //add OSM base tilelayer
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 3,
+	maxZoom: 16,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+    }).addTo(map);
+}
+
+
+function reproject(event) {
+
+}
+
+function showDetail(object) {
+
+}
+
+function createLegend(data, type) {
+
+}
+
+function createBarChart(data) {
+
+}
+
+function createPieChart(data) {
+
+}
